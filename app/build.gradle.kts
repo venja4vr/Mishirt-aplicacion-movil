b4ebc2c247f37f1999c3dadb823785a1ae1e2b78
estaviles <est.aviles@duocuc.cl>
2026-01-15 22:14:31 -0300

    Home: Carrousel, lista vertical de productos con imagenes de prueba

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material:material-icons-extended")
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/mishirt_movil/model/HomeUiState.kt
+++ app/src/main/java/com/example/mishirt_movil/model/HomeUiState.kt
@@ -1,5 +1,19 @@
 package com.example.mishirt_movil.model
 
 data class HomeUiState(
-    val appName: String = "MISHirt"
+    val appName: String = "MISHirt",
+    val carousel: List<CarouselItemUi> = emptyList(),
+    val featuredProducts: List<ProductUi> = emptyList()
 )
+
+data class CarouselItemUi(
+    val title: String,
+    val imageRes: Int
+)
+
+data class ProductUi(
+    val title: String,
+    val price: String,
+    val imageRes: Int
+)
+
